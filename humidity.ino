a1505d44097e7a09bd51eb5d64f48c3496b9750a
ghoshio <gsoham1707@gmail.com>
2023-09-14 12:23:59 +0530

    ded

---
+++ humidity.ino
@@ -0,0 +1,47 @@
+/*
+ * This ESP32 code is created by esp32io.com
+ *
+ * This ESP32 code is released in the public domain
+ *
+ * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-temperature-humidity-sensor
+ */
+
+#include <DHT.h>
+#define DHT_SENSOR_PIN  21 // ESP32 pin GPIO21 connected to DHT22 sensor
+#define DHT_SENSOR_TYPE DHT22
+
+DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht_sensor.begin(); // initialize the DHT sensor
+}
+
+void loop() {
+  // read humidity
+  float humi  = dht_sensor.readHumidity();
+  // read temperature in Celsius
+  float tempC = dht_sensor.readTemperature();
+  // read temperature in Fahrenheit
+  float tempF = dht_sensor.readTemperature(true);
+
+  // check whether the reading is successful or not
+  if ( isnan(tempC) || isnan(tempF) || isnan(humi)) {
+    Serial.println("Failed to read from DHT sensor!");
+  } else {
+    Serial.print("Humidity: ");
+    Serial.print(humi);
+    Serial.print("%");
+
+    Serial.print("  |  ");
+
+    Serial.print("Temperature: ");
+    Serial.print(tempC);
+    Serial.print("°C  ~  ");
+    Serial.print(tempF);
+    Serial.println("°F");
+  }
+
+  // wait a 2 seconds between readings
+  delay(2000);
+}
